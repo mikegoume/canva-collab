715df892e8320023eb171925cc699f41e8b020c9
Michalis Goumenakis <m.goumenakis@i-docs.com>
2025-06-07 22:28:55 +0300

    Remove deprecated forwardRef

--- canva-collab-fe/components/ui/color-picker.tsx
+++ canva-collab-fe/components/ui/color-picker.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { forwardRef, useMemo, useState } from "react";
+import { useState } from "react";
 import { HexColorPicker } from "react-colorful";
 import { VariantProps } from "class-variance-authority";
 
@@ -11,13 +11,13 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { useForwardedRef } from "@/lib/use-forward-ref";
 import { cn } from "@/lib/utils";
 
 interface ColorPickerProps {
   value: string;
   onChange: (value: string) => void;
   onBlur?: () => void;
+  inputRef?: React.Ref<HTMLInputElement>;
 }
 
 interface ButtonProps
@@ -26,60 +26,60 @@ interface ButtonProps
   asChild?: boolean;
 }
 
+type Props = Omit<ButtonProps, "value" | "onChange" | "onBlur"> &
+  ColorPickerProps;
+
 // TODO forwardRef is deprecated in version 19 of React. You can now pass ref directly to components.
-const ColorPicker = forwardRef<
-  HTMLInputElement,
-  Omit<ButtonProps, "value" | "onChange" | "onBlur"> & ColorPickerProps
->(
-  (
-    { disabled, value, onChange, onBlur, name, className, ...props },
-    forwardedRef,
-  ) => {
-    // TODO: why is this hook needed? just pass forwardedRef directly?
-    const ref = useForwardedRef(forwardedRef);
-    const [open, setOpen] = useState(false);
+const ColorPicker = ({
+  disabled,
+  value,
+  onChange,
+  onBlur,
+  name,
+  className,
+  inputRef,
+  ...props
+}: Props) => {
+  const [open, setOpen] = useState(false);
 
-    // TODO: useMemo is really not needed here.
-    const parsedValue = useMemo(() => {
-      return value || "#FFFFFF";
-    }, [value]);
+  const parsedValue = value ?? "#FFFFFF";
 
-    return (
-      <Popover onOpenChange={setOpen} open={open}>
-        <PopoverTrigger asChild disabled={disabled} onBlur={onBlur}>
-          <Button
-            {...props}
-            className={cn("block", className)}
-            name={name}
-            onClick={() => {
-              setOpen(true);
-            }}
-            size="icon"
-            style={{
-              backgroundColor: parsedValue,
-              width: 32,
-              height: 32,
-            }}
-            variant="outline"
-          >
-            <div />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-full">
-          <HexColorPicker color={parsedValue} onChange={onChange} />
-          <Input
-            maxLength={7}
-            onChange={(e) => {
-              onChange(e?.currentTarget?.value);
-            }}
-            ref={ref}
-            value={parsedValue}
-          />
-        </PopoverContent>
-      </Popover>
-    );
-  },
-);
+  return (
+    <Popover onOpenChange={setOpen} open={open}>
+      <PopoverTrigger asChild disabled={disabled} onBlur={onBlur}>
+        <Button
+          {...props}
+          className={cn("block", className)}
+          name={name}
+          onClick={() => {
+            setOpen(true);
+          }}
+          size="icon"
+          style={{
+            backgroundColor: parsedValue,
+            width: 32,
+            height: 32,
+          }}
+          variant="outline"
+        >
+          <div />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-full">
+        <HexColorPicker color={parsedValue} onChange={onChange} />
+        <Input
+          maxLength={7}
+          onChange={(e) => {
+            onChange(e?.currentTarget?.value);
+          }}
+          ref={inputRef}
+          value={parsedValue}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
+
 ColorPicker.displayName = "ColorPicker";
 
 export { ColorPicker };
